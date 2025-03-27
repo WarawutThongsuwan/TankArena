bb93629d43a34de10a15a6262013dc914eceda56
WarawutThongsuwan <radchadokimidon@gmail.com>
2025-03-27 16:53:48 +0700

    Week11

---
+++ Assets/Scripts/Networking/ApplicationController.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ApplicationController : MonoBehaviour
+{
+    [SerializeField] private ClientSingleton clientPrefab;
+    [SerializeField] private HostSingleton hostPrefab;
+    [SerializeField] private NetworkObject playerPrefab;
+    async void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        await LaunchInMode(SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null);
+    }
+
+    private async Task LaunchInMode(bool isDedicatedServer)
+    {
+        if (isDedicatedServer)
+        {
+            
+        }
+        else
+        {
+            HostSingleton hostSingleton = Instantiate(hostPrefab);
+            hostSingleton.CreateHost(playerPrefab);
+
+            ClientSingleton clientSingleton = Instantiate(clientPrefab);
+            bool authenticated = await clientSingleton.CreateClient();            
+
+            if (authenticated)
+            {
+                clientSingleton.GameManager.GoToMenu();
+            }
+        }
+    }
+    
+}
